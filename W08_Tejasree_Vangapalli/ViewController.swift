b4125c55ea37da0b82f5459852dcfd343faab9e9
OSU App Center <osuappcenter@osus-mbp.osustudent.okstate.edu>
2018-10-15 18:12:54 -0500

    Initial Commit

---
+++ W08_Tejasree_Vangapalli/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  W08_Tejasree_Vangapalli
+//
+//  Created by OSU App Center on 10/15/18.
+//  Copyright © 2018 OSU App Center. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
